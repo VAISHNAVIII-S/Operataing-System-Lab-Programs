74ecb7e787686ee4447895366ec7ee6951bb1a17
VAISHNAVI S <vaishnavisathyanarayan10@gmail.com>
2024-08-07 20:09:23 +0530

    Create Grep.c
    Write a program to simulate grep command using system calls

---
+++ Week7/Grep.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+char fn[10],pat[10],temp[200];
+FILE *fp;
+printf("Enter file name\n");
+scanf("%s",fn);
+printf("Enter pattern to be searched\n");
+scanf("%s",pat);
+fp=fopen(fn,"r");
+while(!feof(fp)){
+fgets(temp,100,fp);
+if(strstr(temp,pat))//finds the first occurrence of the substring in the string
+printf("%s",temp);
+}
+fclose(fp);
+}
+
+
+/*fgets(char *str, int n, FILE *stream) reads a line from the specified stream and stores it into the
+string pointed to by str. It stops when either (n-1) characters are read, the newline character is
+read, or the end-of-file is reached, whichever comes first.*/
+
+
+
+/*
+myfile content:
+
+Hello World 
+
+************************
+---------------
+
+Hello World Hello World 
+Operating System
+OS
+Hello World 
+
+*/
+
+
+
+
+/*
+Output: (gcc Grep.c)
+
+Enter file name
+myfile
+Enter pattern to be searched
+******
+************************
+
+
+*/
